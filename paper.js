29372c716d3d92135927462907ef9c23f43dccec
PrekshaGupta <69909564+PrekshaGupta@users.noreply.github.com>
2020-12-04 20:33:53 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -1,19 +1,19 @@
 class Paper{
-    constructor(x,y,diameter){
+    constructor(x,y,radius){
         var options ={
             isStatic:false,
             restitution:0.3,
             friction:0.5,
             density:1.5
         }
-        this.body = Bodies.circle(x,y,diameter,options);
-        this.diameter = diameter;
+        this.body = Bodies.circle(x,y,radius,options);
+        this.radius = radius;
         World.add(world, this.body);
     }
         display(){
         var pos = this.body.position;
         rectMode(CENTER);
         fill(255,0,253);
-        ellipse(pos.x, pos.y, this.diameter);
+        ellipse(pos.x, pos.y, this.radius*2, this.radius*2);
         }
     }
